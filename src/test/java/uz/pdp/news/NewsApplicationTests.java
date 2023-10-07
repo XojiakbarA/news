99bbf1ddd0da6173c6f6936f759d9523e1a87153
khimiki <khimiki@pepsico.uz>
2023-10-07 08:54:27 +0500

    init

---
+++ src/test/java/uz/pdp/news/NewsApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
